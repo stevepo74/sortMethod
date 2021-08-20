d9fceae4615a856a321f4c39c7d2295ba61af353
Steve P. Olson <36646456+stevepo74@users.noreply.github.com>
2021-08-20 09:03:36 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// .Sort()
+
+const planets = [
+  "Mars",
+  "Earth",
+  "Jupiter",
+  "Venus",
+  "Mercury",
+  "Uranus",
+  "Neptune"
+];
+
+console.log(planets.sort());
+
+// works the same on numbers
+
+const numbers = [1, 22, 98, 74, 23, 56, 99];
+
+console.log(numbers.sort());
